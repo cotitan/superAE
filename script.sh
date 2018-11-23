16b02388ee5f663eba47fe82c7b28cbb50717cfb
cotitan <cotitan@outlook.com>
2018-11-23 15:12:13 +0800

    None

--- script.sh
+++ script.sh
@@ -1,1 +1,1 @@
-python preprocess.py -train_src data/lcsts/PART_I.src -train_tgt data/lcsts/PART_I.tgt -valid_src data/lcsts/PART_II.src -valid_tgt data/lcsts/PART_II.tgt -save_data data/lcsts/lcsts.low.share
+python preprocess.py -train_src data/lcsts/PART_I.src -train_tgt data/lcsts/PART_I.tgt -valid_src data/lcsts/PART_II.src -valid_tgt data/lcsts/PART_II.tgt -save_data data/lcsts/lcsts.low.share -share -src_char -tgt_char
